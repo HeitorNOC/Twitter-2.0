774c951ac6ec45159080845bf8e5147d1a0ef709
HeitorNOC <heiitornoc@gmail.com>
2022-05-06 08:35:23 -0300

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon
+} from '@heroicons/react/outline'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <img className='h-10 w-10' src="https://links.papareact.com/drq" />
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ components/SidebarRow.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SidebarRow = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default SidebarRow
